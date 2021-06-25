eba3dddbf2e905d484f4c516a0edc86b92f636fa
Your Name <you@example.com>
2021-06-25 12:08:05 +0500

    Two commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
     $('.header__btn').on('click',function(){
         $('.rightside-menu').removeClass('rightside-menu--close'); //при клике на кнопку btn удаляем класс"close" у menu
-    })
+    });
     $('.rightside-menu--close').on('click',function(){
         $('.rightside-menu').addClass('rightside-menu--close');//при клике на кнопку btn добовляем класс"close" у menu
     });
@@ -15,10 +15,24 @@ $(function () {
     });//слайдер включается
 
 
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows:false,//убираем стрелки
+
+    });//слайдер включается 
+    
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="./images/next.svg" alt="1"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="./images/back.svg" alt="1"></button>',
+
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.bedroom'
         }
     });
 
-})
+});
